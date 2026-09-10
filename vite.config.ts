fe43f7a45dc36adaf5375b0a7226676dfb183899
zwh <zhangweihan008@qq.com>
2026-09-10 23:24:49 +0800

    chroe: 添加开发环境的后端地址的本地代理

--- vite.config.ts
+++ vite.config.ts
@@ -4,19 +4,39 @@ import path from 'path'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-  ],
-  resolve: {
-    alias: { '@': path.resolve(__dirname, 'src') },
-  },
+export default defineConfig(() => {
+  return {
+    plugins: [
+      vue(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [
+          ElementPlusResolver({
+            importStyle: 'sass',
+          }),
+        ],
+      }),
+    ],
+    resolve: {
+      alias: { '@': path.resolve(__dirname, 'src') },
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:8088',
+          changeOrigin: true,
+          rewrite: (path) => path.replace('/api', ''),
+        },
+      },
+    },
+    envDir: './config',
+  }
 })
