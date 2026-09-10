7255cc53319112cc3ea517f98a6b0ce054357289
zwh <zhangweihan008@qq.com>
2026-09-10 23:25:02 +0800

    chroe: 引入mitt

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import { router } from './router'
+import { router, registRouterGlobalEvent } from '@/router/index.ts'
 
 const init = () => {
   const app = createApp(App)
+
+  // 初始化路由
   app.use(router)
+  registRouterGlobalEvent() // 注册route相关的一些全局事件
+
   app.mount('#app')
 }
 
